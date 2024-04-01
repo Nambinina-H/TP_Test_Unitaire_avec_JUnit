15d71c745220eb3811c0167a96738f567a54ed08
Nambinina-H <rasoanaivonambininahasina@gmail.com>
2024-04-01 21:32:45 +0000

    Ajout d'une méthode de test d'égalité ( testEquals() ) dans la classe de test MoneyTest

--- src/test/MoneyTest.java
+++ src/test/MoneyTest.java
@@ -15,4 +15,11 @@ public class MoneyTest {
 		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
 		assertTrue(expected.equals(result)); // comparaison
 	}
+	
+	@Test
+	public void testEquals() {
+	    Money m12CHF = new Money(12, "CHF");
+	    Money anotherM12CHF = new Money(12, "CHF");
+	    assertTrue(m12CHF.equals(anotherM12CHF));
+	}
 }
